7cf99db8d1d078d39fed6577cb3995d2a648c2df
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-10-09 16:59:33 +0200

    Paso 9 terminado

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //Aqui si tiene nombre de funcion ya que vamos a tener más de una
     public function index()
     {
-        //La clase DB de Facades nos permite acceder directamente a la db configurada en el .env
-        $posts = DB::table('posts')->get();
+        //Al ser méodo estático se pueden usar con ::get()
+        $posts = Post::get();
 
         return view('blog', compact('posts'));
     }
---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    //En caso de que el nombre de la tabla no tenga el nombre del modelo en plural habría que especificar el nombre de la tabla
+    //protected $table = 'posts';
+}
