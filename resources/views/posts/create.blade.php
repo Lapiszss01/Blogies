4d46dfcadc9a71a42878beafb5c7b122aec6c9a9
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-10-11 16:17:15 +0200

    Paso14 fin, comprobaciones en lado servidor en el controlador y en la vista del formulario

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,20 +1,26 @@
 <x-layout meta-title="Create a new post" meta-description="Form to create a new post">
     <h1>Create a new Post</h1>
 
-    @foreach($errors->all() as $error){
-        <p>{{$error}}</p>
-    }
+
 
     <form method="POST" action={{route('posts.store')}}>
         @csrf
         <label>
             Title <br>
-            <input type="text" name="title" required>
+            <input type="text" name="title" value="{{old('title')}}">
+            @error('title')
+                <br>
+                <small style="color: red">{{$message}}</small>
+            @enderror
         </label>
         <br>
         <label>
             Body <br>
-            <textarea name="body" required></textarea>
+            <textarea name="body">{{old('body')}}</textarea>
+            @error('body')
+            <br>
+            <small style="color: red">{{$message}}</small>
+            @enderror
         </label>
         <br>
         <button type="submit">Send</button>
