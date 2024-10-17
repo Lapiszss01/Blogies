3642f84bb4270a81b8cfb82e30df4a6e2b407fb2
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-10-17 17:07:57 +0200

    Paso a medias

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             //Documentacion sobre migraciones en laravel.com Database:Migrations
             $table->id();
             $table->string('name');
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
