7eafea4fddefa6db727560b3c855bf51fec3bfc2
Jonathan <jonathan.rbm01@gmail.com>
2024-10-18 10:18:36 +0200

    Al dia todo funcionando

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,6 @@ class Post extends Model
     use HasFactory;
     //En caso de que el nombre de la tabla no tenga el nombre del modelo en plural habría que especificar el nombre de la tabla
     //protected $table = 'posts';
+
+    protected $fillable = ['title', 'body'];
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,37 +1,14 @@
-<x-layout meta-title="Create a new post" meta-description="Form to create a new post">
-    <h1>{{__('Create a new Post')}}</h1>
+<x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
+    <h1>{{ __('Create a new Post') }}</h1>
 
-    <form method="POST" action={{route('posts.store')}}>
+    <form method="POST" action="{{ route('posts.store') }}">
         @csrf
-        <label>
-            {{__('Title')}} <br>
-            <input type="text" name="title" value="{{old('title')}}">
-            @error('title')
-                <br>
-                <small style="color: red">{{$message}}</small>
-            @enderror
-        </label>
-        <br>
-        <label>
-            {{__('Body')}} <br>
-            <textarea name="body">{{old('body')}}</textarea>
-            @error('body')
-            <br>
-            <small style="color: red">{{$message}}</small>
-            @enderror
-        </label>
-        <br>
-        <label>
-            {{__('Author')}} <br>
-            <textarea name="author">{{old('author')}}</textarea>
-            @error('author')
-            <br>
-            <small style="color: red">{{$message}}</small>
-            @enderror
-        </label>
-        <br>
-        <button type="submit">{{__('Send')}}</button>
-        <br>
+
+        @include('posts.form-fields')
+
+        <br />
+        <button type="submit">{{ __('Send') }}</button>
+        <br />
     </form>
-    <a href={{route('posts.index')}}>{{__('Back')}}</a>
+    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
 </x-layout>
